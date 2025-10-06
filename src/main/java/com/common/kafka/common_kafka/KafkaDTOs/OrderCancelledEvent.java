f90c4bb1026a4cf7fb664b06934472bc5ee7d18a
PavithranathanVM <pavithranathanvm2111@gmail.com>
2025-10-06 20:10:54 +0530

    initial

---
+++ src/main/java/com/common/kafka/common_kafka/KafkaDTOs/OrderCancelledEvent.java
@@ -0,0 +1,20 @@
+package com.common.kafka.common_kafka.KafkaDTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderCancelledEvent {
+
+    private UUID orderId;
+    private UUID customerId;
+    private String reason;
+    private LocalDateTime cancelledAt;
+
+}
